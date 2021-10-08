8446476d86a99b95a6673797761c03f6073357db
Maduka Nuwantha <mbnuwantha90@gmail.com>
2021-10-08 21:40:09 +0530

    Project structure init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CargillsOnline());
+}
+
+class CargillsOnline extends StatelessWidget {
+  const CargillsOnline({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
